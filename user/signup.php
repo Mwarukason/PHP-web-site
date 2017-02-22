9b372aa6441e17bed2c57e2864552587d7ff353e
Mwarukason <a.mwaruka93@gmail.com>
2017-02-23 02:27:58 +0800

    update the signup div to match with form

--- user/signup.php
+++ user/signup.php
@@ -9,47 +9,49 @@
 	</div>
 
 	<div class="row signup">
-		<h3>Welcome Our Dearest User, Please Sign Up</h3>
-	</div>
+		<div class="row">
+			<h3>Welcome Our Dearest User, Please Sign Up</h3>
+		</div>
 	
-	<!-- add bostrap element form-horizontal-->
-	<form action="" method="post" class=".form-horizontal">
+		<!-- add bostrap element form-horizontal-->
+		<form action="" method="post" class=".form-horizontal">
 		
-		<div class="row form-group input_group">
-			<label for="" class="col-sm-2">Email</label>
-			<div class="col-sm-10">
-				<input type="text" name="email" id="email" class="form-control">
+			<div class="row form-group input_group">
+				<label for="" class="col-sm-2">Email</label>
+				<div class="col-sm-10">
+					<input type="text" name="email" id="email" class="form-control">
+				</div>
 			</div>
-		</div>
 
-		<div class="row form-group input_group">
-			<label for="" class="col-sm-2">Firstname</label>
-			<div class="col-sm-10">
-				<input type="text" name="Firstname" id="Firstname" class="form-control">
+			<div class="row form-group input_group">
+				<label for="" class="col-sm-2">Firstname</label>
+				<div class="col-sm-10">
+					<input type="text" name="Firstname" id="Firstname" class="form-control">
+				</div>
 			</div>
-		</div>
 
-		<div class="row form-group input_group">
-			<label for="" class="col-sm-2">Lastname</label>
-			<div class="col-sm-10">
-				<input type="text" name="Lastname" id="Lastname" class="form-control">
+			<div class="row form-group input_group">
+				<label for="" class="col-sm-2">Lastname</label>
+				<div class="col-sm-10">
+					<input type="text" name="Lastname" id="Lastname" class="form-control">
+				</div>
 			</div>
-		</div>
 
-		<div class="row form-group input_group">
-			<label for="" class="col-sm-2">Password</label>
-			<div class="col-sm-10">
-				<input type="password" name="password" id="password" class="form-control">
+			<div class="row form-group input_group">
+				<label for="" class="col-sm-2">Password</label>
+				<div class="col-sm-10">
+					<input type="password" name="password" id="password" class="form-control">
+				</div>
 			</div>
-		</div>
 
-		<div class="row form-group" style="margin: 0px 10px 20px 10px">
-			<div class="col-xs-12">
-				<input type="submit" name="submit" id="submit" class="form-control">
+			<div class="row form-group" style="margin: 0px 10px 20px 10px">
+				<div class="col-xs-12">
+					<input type="submit" name="submit" id="submit" class="form-control">
+				</div>
 			</div>
-		</div>
 
-	</form>
+		</form>
+	</div>
 
 
 </div>
