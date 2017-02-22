47a51bddb0f63427680cb2a3435fd3bfd494882d
Mwarukason <a.mwaruka93@gmail.com>
2017-02-22 22:09:38 +0800

    added nav ba and content nav

--- index.php
+++ index.php
@@ -13,10 +13,54 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-
+	<link rel="stylesheet" type="text/css" href="./css/custome.css">
 
 </head>
+
 <body>
+<nav class="navbar" style="background-color: #004080;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand crud" href="#">PHP APPLICATION</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+	<ul class="nav nav-tabs">
+		<li><a data-toggle="tab" href="#index">Index</a></li>
+		<li><a data-toggle="tab" href="#login">Login</a></li>
+		<li><a data-toggle="tab" href="#signup">Sign Up</a></li>
+		<li><a data-toggle="tab" href="#admin">Admin</a></li>
+	</ul>
+
+	<!--creating access for each nav tab and create its content fade in to appear when clicked-->
+	<div class="tab-content">
+		<div id="index" class="tab-pane fade in">
+			<p>Login</p>
+		</div>
+
+		<div id="login" class="tab-pane fade ">
+			<p>Login</p>
+		</div>
+
+		<div id="signup" class="tab-pane fade in">
+			<p>Sign Up</p>
+		</div>
+
+		<div id="admin" class="tab-pane fade in">
+			<p>Admin</p>
+		</div>
+
+	</div>
+
+</div>
+
 
 </body>
 </html>
